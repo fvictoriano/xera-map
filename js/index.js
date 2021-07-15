0cfe92dc604d3ca89d585da44ec7d6b35cf3a476
fvictoriano <fillipe_victoriano@live.com>
2021-07-15 04:20:32 -0300

    location update & towers

--- js/index.js
+++ js/index.js
@@ -28,6 +28,7 @@ var gas = L.layerGroup().addTo(map);
 var compounds = L.layerGroup().addTo(map);
 var helis = L.layerGroup().addTo(map);
 var bunkers = L.layerGroup().addTo(map);
+var towers = L.layerGroup().addTo(map);
 
 var overlays = {
     "Cities": cities,
@@ -40,6 +41,7 @@ var overlays = {
     "Reactor Events": reactors,
     "Helicopter Crashes": helis,
     "Bunker Events": bunkers,
+    "Towers": towers,
 }
 
 //icons
@@ -79,29 +81,33 @@ var bunkerIcon = L.icon({
     iconSize: [20, 20], 
 });
 
+var towerIcon = L.icon({
+    iconUrl: 'img/icons/tower.png',
+    iconSize: [20, 20],
+});
+
 
 //markers
-L.marker([578, 321], {icon: atvIcon}).addTo(atv)
-L.marker([973, 374], {icon: atvIcon}).addTo(atv)
-L.marker([1568, 234], {icon: atvIcon}).addTo(atv)
-L.marker([1287, 820], {icon: atvIcon}).addTo(atv)
-L.marker([1215, 1159], {icon: atvIcon}).addTo(atv)
-L.marker([1271, 1440], {icon: atvIcon}).addTo(atv)
-L.marker([445, 846], {icon: atvIcon}).addTo(atv)
-L.marker([788, 1347], {icon: atvIcon}).addTo(atv)
-L.marker([1430, 1439], {icon: atvIcon}).addTo(atv)
-L.marker([749, 1316], {icon: atvIcon}).addTo(atv)
-L.marker([1010, 446], {icon: atvIcon}).addTo(atv)
+L.marker([578, 321], {icon: atvIcon}).addTo(atv);
+L.marker([983, 382], {icon: atvIcon}).addTo(atv);//bakersfield left (confirmed)
+L.marker([1568, 234], {icon: atvIcon}).addTo(atv);
+L.marker([1287, 820], {icon: atvIcon}).addTo(atv);// near hedgewood (confirmed)
+L.marker([1215, 1159], {icon: atvIcon}).addTo(atv);//winchester north (confirmed)
+L.marker([1271, 1440], {icon: atvIcon}).addTo(atv);
+L.marker([788, 1347], {icon: atvIcon}).addTo(atv);
+L.marker([749, 1316], {icon: atvIcon}).addTo(atv);// arrow town left (confirmed)
+L.marker([1010, 446], {icon: atvIcon}).addTo(atv);
+L.marker([958, 1012], {icon: atvIcon}).addTo(atv);//center winchester (confirmed)
 
-L.marker([435, 846], {icon: carIcon}).addTo(cars)
-L.marker([767, 1377], {icon: carIcon}).addTo(cars)
-L.marker([1008, 1049], {icon: carIcon}).addTo(cars)
-L.marker([1050, 1214], {icon: carIcon}).addTo(cars)
-L.marker([1157, 931], {icon: carIcon}).addTo(cars)
-L.marker([1389, 1424], {icon: carIcon}).addTo(cars)
-L.marker([1338, 1446], {icon: carIcon}).addTo(cars)
-L.marker([965, 1003], {icon: carIcon}).addTo(cars)
-L.marker([453, 967], {icon: carIcon}).addTo(cars)
+L.marker([435, 846], {icon: carIcon}).addTo(cars);//melrose left (confirmed)
+L.marker([767, 1377], {icon: carIcon}).addTo(cars);//arrow town right (confirmed)
+L.marker([1013, 1035], {icon: carIcon}).addTo(cars);//center top winchester (confirmed)
+L.marker([1063, 1213], {icon: carIcon}).addTo(cars);//right winchester (confirmed)
+L.marker([1157, 931], {icon: carIcon}).addTo(cars);//top winchester (confirmed)
+//L.marker([1389, 1424], {icon: carIcon}).addTo(cars); not confirmed
+L.marker([1338, 1446], {icon: carIcon}).addTo(cars);//gas station on moutain view (confirmed)
+L.marker([453, 967], {icon: carIcon}).addTo(cars);
+L.marker([1421, 1439], {icon: carIcon}).addTo(cars);//moutain view north (confirmed)
 
 L.marker([912, 1480], {icon: gasIcon}).addTo(gas);
 L.marker([1432, 1130], {icon: gasIcon}).addTo(gas);
@@ -135,6 +141,24 @@ L.marker([472, 1722], {icon: heliIcon}).addTo(helis);
 L.marker([597, 534], {icon: bunkerIcon}).addTo(bunkers);
 L.marker([1816, 1372], {icon: bunkerIcon}).addTo(bunkers);
 L.marker([1254, 133], {icon: bunkerIcon}).addTo(bunkers);
+
+L.marker([1652, 795], {icon: towerIcon}).addTo(towers);
+L.marker([1420, 1747], {icon: towerIcon}).addTo(towers);
+L.marker([984, 1436], {icon: towerIcon}).addTo(towers);
+L.marker([816, 1023], {icon: towerIcon}).addTo(towers);
+L.marker([1335, 1260], {icon: towerIcon}).addTo(towers);
+L.marker([1418, 596], {icon: towerIcon}).addTo(towers);
+L.marker([845, 1689], {icon: towerIcon}).addTo(towers);
+L.marker([649, 718], {icon: towerIcon}).addTo(towers);
+L.marker([368, 833], {icon: towerIcon}).addTo(towers);
+L.marker([627, 1684], {icon: towerIcon}).addTo(towers);
+L.marker([1398, 954], {icon: towerIcon}).addTo(towers);
+L.marker([1724, 205], {icon: towerIcon}).addTo(towers);
+L.marker([368, 833], {icon: towerIcon}).addTo(towers);
+L.marker([368, 833], {icon: towerIcon}).addTo(towers);
+
+
+
 
 
 // AREA NAMES
