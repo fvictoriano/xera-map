a82f704344cdf162515d04f6561f68d0086beddb
fvictoriano <fillipe_victoriano@live.com>
2021-07-30 22:36:31 -0300

    atv update/ melrose

--- js/index.js
+++ js/index.js
@@ -106,6 +106,7 @@ L.marker([749, 1316], {icon: atvIcon}).addTo(atv);// arrow town left (confirmed)
 L.marker([1010, 446], {icon: atvIcon}).addTo(atv);
 L.marker([958, 1012], {icon: atvIcon}).addTo(atv);//center winchester (confirmed)
 L.marker([1389, 1424], {icon: atvIcon}).addTo(atv);
+L.marker([454, 965], {icon: atvIcon}).addTo(atv);// south melrose (confirmed)
 
 L.marker([435, 846], {icon: carIcon}).addTo(cars);//melrose left (confirmed)
 L.marker([767, 1377], {icon: carIcon}).addTo(cars);//arrow town right (confirmed)
@@ -113,7 +114,6 @@ L.marker([1013, 1035], {icon: carIcon}).addTo(cars);//center top winchester (con
 L.marker([1063, 1213], {icon: carIcon}).addTo(cars);//right winchester (confirmed)
 L.marker([1157, 931], {icon: carIcon}).addTo(cars);//top winchester (confirmed)
 L.marker([1338, 1446], {icon: carIcon}).addTo(cars);//gas station on moutain view (confirmed)
-L.marker([453, 967], {icon: carIcon}).addTo(cars);
 L.marker([1421, 1439], {icon: carIcon}).addTo(cars);//moutain view north (confirmed)
 L.marker([1613, 210], {icon: carIcon}).addTo(cars);//henderson north (confirmed)
 
